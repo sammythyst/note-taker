caba507b7e2047a583922e7452dabb63cf1518de
sasw <saswain96@gmail.com>
2023-01-04 16:17:55 -0600

    get and post routes added

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
+const uniqid = require('uniqid');
 
-const uuid = require('./helpers/uuid');
-const PORT = 8080;
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true }));
 
+app.use(express.json());
 app.use(express.static('public'));
 
 // GET route for homepage
@@ -22,6 +21,24 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// GET information from db
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (error, data) => {
+        res.send(data);
+    });
+});
+
+// POST note to db
+app.post('/api/notes', (req,res) => {
+    fs.readFile('./db/db.json', (error, data) => {
+        const notes = JSON.parse(data);
+        notes.push({...req.body, id: uniqid() });
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (error, data) => {
+            res.send(req.body);
+        });
+    });
+});
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
